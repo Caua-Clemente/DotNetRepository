f25c80e1e4d37bcbb9dd3a3842a5692e57dfd970
Cauã Clemente Lima <cauaclementelima@gmail.com>
2023-11-07 17:20:35 -0300

    Primeira 'pratica'

--- Semana1/Program.cs
+++ Semana1/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int intX = int.MaxValue;
+double doubleX = double.MaxValue;
+float floatX = float.MaxValue;
+long longX = long.MaxValue;
+decimal decimalX = decimal.MaxValue;
+
+Console.WriteLine("Valores máximos de:");
+Console.WriteLine($"Int: {intX}");
+Console.WriteLine($"Double: {doubleX}");
+Console.WriteLine($"float: {floatX}");
+Console.WriteLine($"Long: {longX}");
+Console.WriteLine($"Decimal: {decimalX}");
