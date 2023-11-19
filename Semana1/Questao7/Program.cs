5573e7d6ad5ddd55a35e9180edd7ff65f50efed7
Caua-Clemente <cauaclementelima@gmail.com>
2023-11-19 17:17:12 -0300

    Subindo a lista .NET-P001 completa

---
+++ Semana1/Questao7/Program.cs
@@ -0,0 +1,7 @@
+﻿bool condicao1 = true;
+bool condicao2 = false;
+
+if(condicao1 && condicao2)
+    Console.WriteLine("As duas condicoes sao verdadeiras");
+else
+    Console.WriteLine("Ao menos uma das condicoes nao e verdadeira");
---
+++ Semana1/Questao8/Program.cs
@@ -0,0 +1,15 @@
+﻿int num1 = 7;
+int num2 = 3;
+int num3 = 10;
+
+if(num1 > num2)
+    Console.WriteLine("Num1 e maior que Num2");
+else
+    Console.WriteLine("Num1 nao e maior que Num2");
+
+if(num3 == (num1 + num2))
+    Console.WriteLine("Num3 e igual a Num1 + Num2");
+else
+    Console.WriteLine("Num3 nao e igual a Num1 + Num2");
+
+    
