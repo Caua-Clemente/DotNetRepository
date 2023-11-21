205acf23f9841954c5a7fba02cd9b0d264cb29e4
Caua-Clemente <cauaclementelima@gmail.com>
2023-11-20 22:54:40 -0300

    Alterando conteudo da branch para envio

--- Semana1/Questao4/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿int x = 10;
-int y = 3;
-
-Console.WriteLine("x + y = " + (x + y));
-Console.WriteLine("x - y = " + (x - y));
-Console.WriteLine("x * y = " + (x * y));
-Console.WriteLine("x / y = " + (x / y));
--- Semana1/Questao6/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿string str1 = "Hello";
-string str2 = "World";
-
-if(str1 == str2)
-    Console.WriteLine("As strings sao iguais");
-else
-    Console.WriteLine("As strings nao sao iguais");
