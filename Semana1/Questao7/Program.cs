62202a91997f542525f0af7683c4d89cfe646adc
Caua-Clemente <cauaclementelima@gmail.com>
2023-11-25 18:38:18 -0300

    Criando branch para P003

--- Semana1/Questao7/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿bool condicao1 = true;
-bool condicao2 = false;
-
-if(condicao1 && condicao2)
-    Console.WriteLine("As duas condicoes sao verdadeiras");
-else
-    Console.WriteLine("Ao menos uma das condicoes nao e verdadeira");
--- Semana3/ExemplosAula/atvLambda01/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿string person = Console.ReadLine() ?? "";
-
-Func<string, int, string> isBiggerThan = (string s, int x) => s.Length > x ? "Yes" : "NO";
-var size = 5; 
-Console.WriteLine($"The text {person} has more than {size} chars? {isBiggerThan(person, size)}");
